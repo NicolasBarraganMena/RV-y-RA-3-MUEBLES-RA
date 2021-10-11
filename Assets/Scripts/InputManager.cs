d84f589339a2f0bf6d87f6305720ea4c1d50843f
Nicolas Barragan Mena <nicolas.barragan@upb.edu.co>
2021-10-10 22:04:19 -0500

    Crear código Input Manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject arObject;
+    [SerializeField] private Camera arCamera;
+    [SerializeField] private ARRaycastManager raycastManager;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)) //Si se da click izquierdo del mouse
+        {
+            Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
+            if (raycastManager.Raycast(ray, hits))
+            {
+                Pose pose = hits[0].pose;
+                Instantiate(arObject, pose.position, pose.rotation);
+            }
+        }
+
+    }
+}
